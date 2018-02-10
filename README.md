ba9aae83453fead7a7e04a3c51c34fbafb3032b3
BasHouse <bashouse@hotmail.com>
2018-02-10 02:27:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# bascoin
---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid BYTECOIN_NETWORK = { {  0x4e, 0x1f, 0x96, 0xcf, 0x65, 0x8c, 0xbf, 0xa9, 0x74, 0xcf, 0x93, 0x81, 0xfc, 0xb1, 0x39, 0x35  } };
+
+}
+
